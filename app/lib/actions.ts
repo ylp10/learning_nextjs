49ad9ea084b3c53bbfafff1b91c042b2a06c60ac
Yunkai <yunkailin10@gmail.com>
2024-08-06 13:51:04 +0200

    Up to chapter 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import {z} from 'zod';
 import {sql} from '@vercel/postgres';
 import {revalidatePath} from 'next/cache';
 import {redirect} from 'next/navigation';
+import {signIn} from '@/auth';
+import { AuthError } from 'next-auth';
 
 export type State = {
     errors?: {
@@ -32,7 +34,7 @@ export async function createInvoice(prevState: State, formData: FormData){
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
-    
+
     if(!validatedFields.success){
         return {
             errors: validatedFields.error.flatten().fieldErrors,
@@ -103,3 +105,24 @@ export async function deleteInvoice(id: string){
         return {message: 'Database error: Unable to delete invoice'};
     }
 }
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+      // log in successful
+      alert('Logged in successfully');
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
+  }
